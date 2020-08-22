e5c9a93afd74b7c793802f5febaee649998e12c0
sametweb <sametmutevellioglu@gmail.com>
2020-08-22 01:33:43 -0700

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,47 @@
-{
-  "presets": [["react-app", { "absoluteRuntime": false }]]
-}
+// module.exports = {
+//   presets: [["react-app", { absoluteRuntime: false }]],
+// };
+
+module.exports = {
+  env: {
+    test: {
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            modules: "commonjs",
+            debug: false,
+          },
+        ],
+        "@babel/preset-flow",
+        "@babel/preset-react",
+      ],
+      plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties",
+      ],
+    },
+    production: {
+      presets: [
+        ["@babel/preset-env", { modules: false }],
+        "@babel/preset-flow",
+        "@babel/preset-react",
+      ],
+      plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties",
+      ],
+    },
+    development: {
+      presets: [
+        ["@babel/preset-env", { modules: false }],
+        "@babel/preset-flow",
+        "@babel/preset-react",
+      ],
+      plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties",
+      ],
+    },
+  },
+};
